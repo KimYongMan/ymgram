603cb1a3da08d07e392ba4eaad89b3795503cd3b
yongmankim <heaven0223@naver.com>
2019-03-17 10:40:16 +0900

    super user

--- config/settings/base.py
+++ config/settings/base.py
@@ -71,6 +71,7 @@ THIRD_PARTY_APPS = [
 ]
 LOCAL_APPS = [
     'ymgram.users.apps.UsersAppConfig',
+    'ymgram.images.apps.ImagesConfig',
     # Your stuff: custom apps go here
 ]
 # https://docs.djangoproject.com/en/dev/ref/settings/#installed-apps
